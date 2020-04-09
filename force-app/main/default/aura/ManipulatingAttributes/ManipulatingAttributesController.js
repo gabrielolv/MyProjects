dd505185e913e2ac67ecd2cc28dfc1539459c066
Gabriel <52607758+gabrielolv@users.noreply.github.com>
2020-04-08 21:51:53 -0300

    commmit date 08/04/2020

--- force-app/main/default/aura/ManipulatingAttributes/ManipulatingAttributesController.js
+++ force-app/main/default/aura/ManipulatingAttributes/ManipulatingAttributesController.js
@@ -1,5 +1,14 @@
 ({
-    myAction : function(component, event, helper) {
-
+    handleClick : function(component, event, helper) {
+        var msg = component.get("v.FirstName");
+        if (msg == 'SEU NOME') {
+            //alert("FUNCIONOU CARAMBA!!!!");
+            component.set("v.FirstName", 'Olá, ' + msg + '!');
+        }else{
+            component.set("v.FirstName", 'SEU NOME');
+        }
+    },
+    doInit : function(component, event, helper){
+        alert("The component is initialized");
     }
 })
